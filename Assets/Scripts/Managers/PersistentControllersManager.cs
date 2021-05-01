1d6a741894a0794e2bdf9e17d1a19167a434aa6f
ricardj <43671359+ricardj@users.noreply.github.com>
2021-05-01 13:34:14 +0200

    Improved project structure.

---
+++ Assets/Scripts/Managers/PersistentControllersManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentControllersManager : MonoBehaviour
+{
+    public static PersistentControllersManager instance;
+
+    [RuntimeInitializeOnLoadMethod]
+    public static void Initialize()
+    {
+        string prefabName = "M - PersistentManagers";
+        GameObject effectsController = Resources.Load(prefabName) as GameObject;
+        GameObject instantiatedDevPanel = Instantiate(effectsController);
+        DontDestroyOnLoad(instantiatedDevPanel);
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(this);
+        }
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+}
